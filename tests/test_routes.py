c80307423764496eaad455b70f549d023a1800e1
Vicky <vickyks@hotmail.com>
2022-04-08 16:39:28 +0000

    pytests are failing. Changed the models slightly

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,8 @@
-from models import Users
 from flask_testing import TestCase
+from application.models import Users 
 from application import app, db
+from flask import render_template, redirect, url_for, request
+
 
 class TestBase(TestCase):
 
@@ -11,10 +13,14 @@ class TestBase(TestCase):
     def setUp(self):
         db.create_all()
 
-        test_User = User("1", "Vicky_Jones", "")
+        test_User = Users(user_name="Vicky_Jones", loans=1000, password="Groovy123", property=1000000, cash=500000, investments=200000)
+
+        db.session.add(test_User)
+        db.session.commit()
 
     def tearDown(self):
-        pass
+        db.session.remove()
+        db.drop_all()
 
     
 class TestViews(TestBase):
@@ -25,28 +31,28 @@ class TestViews(TestBase):
 
     def test_add_profile(self):
         response = self.client.get(url_for('add_profile'))
-         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-     def test_add_profile(self):
+    def test_add_profile(self):
         response = self.client.get(url_for('add_debt'))
-         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-     def test_add_profile(self):
-        response = self.client.get(url_for('update_profile'))
-         self.assertEqual(response.status_code, 200)
+    def test_add_profile(self):
+        response = self.client.get(url_for('update_profile', 1))
+        self.assertEqual(response.status_code, 302)
 
 
     def test_add_profile(self):
-        response = self.client.get(url_for('update_debt'))
-         self.assertEqual(response.status_code, 200)
+        response = self.client.get(url_for('update_debt', 1))
+        self.assertEqual(response.status_code, 302)
 
     def test_add_profile(self):
-        response = self.client.get(url_for('view_networth'))
-         self.assertEqual(response.status_code, 200)    
+        response = self.client.get(url_for('view_networth', 1))
+        self.assertEqual(response.status_code, 302)    
 
     def test_add_profile(self):
-        response = self.client.get(url_for('delete_profile'))
-         self.assertEqual(response.status_code, 200)
+        response = self.client.get(url_for('delete_profile', 1))
+        self.assertEqual(response.status_code, 302)
 
 
 
